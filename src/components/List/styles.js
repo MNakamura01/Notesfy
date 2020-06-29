ed7e739fe192ae4a3a19dacecf957269c560902c
mnakamura01 <mrnnakamura01@gmail.com>
2020-06-29 01:00:01 -0300

    Iniciando Drag'n Drop

--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -9,6 +9,8 @@ export const Container = styled.div`
     flex-shrink: 0; //habilidade do componente "reduzir" mais doque o normal
     flex-basis: 320px; //tamanho base do elemento 
 
+    opacity: ${props => props.done ? 0.6 : 1};
+
     & + div {
         border-left: 1px solid rgba(0,0,0,0.05);
     }
